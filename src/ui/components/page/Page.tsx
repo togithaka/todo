b4292293ded50e0708c2cb606331cbbf46bdf7a9
togithaka <togithaka@gmail.com>
2025-12-23 07:58:08 +0300

    refactor(`src/ui/components/page`): pass main layout styles in `Page.tsx`
    Updated the Page component to apply a page-specific layout class to the Main component in `Page.tsx`.
    
    This aligns the Main layout with the grid-based page structure and ensures consistent positioning alongside the header.

--- src/ui/components/page/Page.tsx
+++ src/ui/components/page/Page.tsx
@@ -1,12 +1,12 @@
 import { Header, Main } from '@ui/components/layout';
-import { HeaderStyles } from '@ui/styles/layout';
+import { HeaderStyles, MainStyles } from '@ui/styles/layout';
 import { PageStyles } from '@ui/styles/page';
 
 export default function Page() {
   return (
     <div className={PageStyles.Page}>
       <Header className={HeaderStyles.Page} />
-      <Main />
+      <Main className={MainStyles.Page} />
     </div>
   );
 }
